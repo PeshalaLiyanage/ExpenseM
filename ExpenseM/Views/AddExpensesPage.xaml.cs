306d511349a62a85368b039b3907380e931a46dc
PeshalaLiyanage <peshala1996@gmail.com>
2021-01-07 12:10:01 +0530

    Expense add row implemented

--- ExpenseM/Views/AddExpensesPage.xaml.cs
+++ ExpenseM/Views/AddExpensesPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ExpenseM.UserControls;
 
 namespace ExpenseM.Views
 {
@@ -20,9 +21,39 @@ namespace ExpenseM.Views
   /// </summary>
   public partial class AddExpensesPage : Page
   {
+
+    private int totalRowsCount = 0;
+    private const int MaxRowsCount = 12;
+
+    List<ExpensesAddRow> labeledTextboxes = new List<ExpensesAddRow>();
     public AddExpensesPage()
     {
       InitializeComponent();
+     //create user control here and after creating it, move it to the user control file(expense add)
+    }
+
+    private void AddRowsBtn_Click(object sender, RoutedEventArgs e)
+    {
+      int rowsCount = int.Parse(this.RowCountBox.Text);
+     
+
+      if (rowsCount<= MaxRowsCount && (totalRowsCount += rowsCount) <= MaxRowsCount)
+      {
+        //totalRowsCount += rowsCount;
+        for (int i = 0; i < rowsCount; i++)
+        {
+          ExpensesAddRow expensesAddRow = new ExpensesAddRow();
+          Thickness thickness = new Thickness();
+          thickness.Top = 10;
+          expensesAddRow.Margin = thickness;
+          this.MainPannel.Children.Add(expensesAddRow);
+        }
+
+      }
+      else
+      {
+        MessageBox.Show("You can add only maximum 12 records at once.");
+      }
     }
   }
 }
